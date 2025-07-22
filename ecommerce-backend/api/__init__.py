a14d05a8303c7bdcaa6e35f52f284afe968aa5c6
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-07-22 17:55:01 +0100

    Add initial API structure and empty module files
    - Created __init__.py files for the main package, core, and schemas directories.
    - Implemented the API router in ecommerce-backend/api/__init__.py, integrating various routers for authentication, products, orders, cart, payments, address, analytics, and admin functionalities.

---
+++ ecommerce-backend/api/__init__.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from .admin import admin_router
+from .products import product_router
+from .payments import payment_router
+from .orders import router as orders_router
+from .cart import router as cart_router
+from .auth import router as auth_router
+from .admin import admin_router
+from .address import router as address_router
+from .analytics import router as analytics_router
+
+
+api_version_one = APIRouter(prefix="/api/v1")
+api_version_one.include_router(auth_router)
+api_version_one.include_router(product_router)
+api_version_one.include_router(orders_router)
+api_version_one.include_router(cart_router)
+api_version_one.include_router(payment_router)
+api_version_one.include_router(address_router)
+api_version_one.include_router(analytics_router)
+api_version_one.include_router(admin_router)
