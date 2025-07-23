5fd9736cb6347d263bd126f76acfff20de7c458c
Ajiboye Adeleye Pius <ajiboyeadeleye080@gmail.com>
2025-07-23 18:54:14 +0100

    added profile router

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -8,10 +8,12 @@ from .auth import router as auth_router
 from .admin import admin_router
 from .address import router as address_router
 from .analytics import router as analytics_router
+from api.profile import router as profile_router
 
 
 api_version_one = APIRouter(prefix="/api/v1")
 api_version_one.include_router(auth_router)
+api_version_one.include_router(profile_router)
 api_version_one.include_router(product_router)
 api_version_one.include_router(orders_router)
 api_version_one.include_router(cart_router)
